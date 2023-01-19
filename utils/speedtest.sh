7a592f79649945684a6676a70ea4098cfe62df28
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-01-19 18:21:30 +1100

    speedtest.sh

--- utils/speedtest.sh
+++ utils/speedtest.sh
@@ -5,6 +5,8 @@
 MEASURECMD='candump vcan0 | tqdm --bytes > /dev/null'
 RUNTIME=5s
 
+echo "Speedtest comparison of python-can to JorzaCAN"
+
 echo "Running spam-pycan.py for 5s"
 timeout $RUNTIME ./spam-pycan.py &
 timeout $RUNTIME bash -c "${MEASURECMD}"
@@ -15,4 +17,4 @@ echo "Running spam-jorzacan.py for 5s"
 timeout $RUNTIME ./spam-jorzacan.py & 
 timeout $RUNTIME bash -c "${MEASURECMD}"
 
-
+echo ""
