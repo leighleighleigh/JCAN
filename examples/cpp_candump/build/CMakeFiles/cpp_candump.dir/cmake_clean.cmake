96ad732aa145946ffef7eea06df04031bb9b08b9
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-01-21 14:23:12 +1100

    Moved examples to top-level, re-symlinked them to /out

---
+++ examples/cpp_candump/build/CMakeFiles/cpp_candump.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_candump.dir/include/jorzacan/jorzacan.cc.o"
+  "CMakeFiles/cpp_candump.dir/include/jorzacan/jorzacan.cc.o.d"
+  "CMakeFiles/cpp_candump.dir/src/main.cpp.o"
+  "CMakeFiles/cpp_candump.dir/src/main.cpp.o.d"
+  "cpp_candump"
+  "cpp_candump.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_candump.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/cpp_cansend/build/CMakeFiles/cpp_cansend.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cpp_cansend.
+# This may be replaced when dependencies are built.
