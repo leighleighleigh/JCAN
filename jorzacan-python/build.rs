243325e6ea9ef5beeaccca76d7d4cb5032d86136
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-01-21 16:42:00 +1100

    PYO3_NO_PYTHON flag, other tweaks to optimise wheel compat.

--- jorzacan-python/build.rs
+++ jorzacan-python/build.rs
@@ -1,12 +1,8 @@
 #[allow(unused_must_use)]
+
 fn main() {
-    // Run 'maturin build' shell command. This builds the python wheels for distribution
-    // This is a bit of a hack, but it works
-    // Make sure to first change the directory to that of this cargo crate
-    // This is because maturin will look for the Cargo.toml file in the current directory
-    // and will fail if it is not there
-    // std::env::set_current_dir(std::path::Path::new(std::env::var("CARGO_MANIFEST_DIR").unwrap().as_str())).unwrap();
-    // std::process::Command::new("maturin").arg("build").output().unwrap();
-    // Tell cargo to rerun this script if any of the files we copied change
-    // println!("cargo:rerun-if-changed=src/lib.rs");
+    // Set environment variable PYO3_NO_PYTHON
+    // to disable the automatic detection of Python
+    std::env::set_var("PYO3_NO_PYTHON", "1");
+    pyo3_build_config::add_extension_module_link_args();
 }
