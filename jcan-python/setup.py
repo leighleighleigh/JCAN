54a0a4da0df5d33e7c28063129f3855cf6ffda97
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-01-22 13:44:04 +1100

    Added some pre-flight checks for dev env setup

--- jcan-python/setup.py
+++ jcan-python/setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="jcan",
-    version="0.1.1",
+    version="0.1.2",
     packages=["jcan"],
     zip_safe=False,
     rust_extensions=[
