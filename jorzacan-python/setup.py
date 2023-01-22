3aaa2e7ec06fd263ae57c7be1a0f4352e3c599b0
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-01-22 12:17:53 +1100

    Bumped version to 0.1.1

--- jorzacan-python/setup.py
+++ jorzacan-python/setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="jorzacan",
-    version="0.1.0",
+    version="0.1.1",
     packages=["jorzacan"],
     zip_safe=False,
     rust_extensions=[
