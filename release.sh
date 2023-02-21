5cdc60499c725d4ddffce15dae0b0da84784ed23
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-02-22 08:37:07 +1100

    Improved release script to generate better tag names

--- release.sh
+++ release.sh
@@ -16,24 +16,45 @@ mkdir "${SCRIPT_DIR}/release"
 # Or it will be a combination of tag+commit-hash 
 # if the current commit is not tagged. (such as v0.1.6-1-gf3c5c5c)
 # If the working tree is dirty (uncommited changes), it will append -dirty
-# to the end of the tag. This ensures the build environment is tidy.
+# to the end of the tag. This ensures the build artifacts are clearly labelled.
 GIT_DESCRIBED_TAG=$(git describe --tags --match 'v*' --dirty)
 
-# Get the current git tag
-GIT_TAG=$(git tag -l | tail -n1)
+# Get the latest annotated tag that starts with 'v'
+# e.g v0.1.5
+GIT_LATEST_TAG=$(git tag -l 'v*' | tail -n1)
 
-# If the current git tag is not a release tag, exit
-if [[ "${GIT_TAG}" != "v"* ]];
+# If the argument --latest-tag is passed, use the latest annotated tag
+# instead of the git describe tag.
+if [[ "$1" == "--latest-tag" ]];
 then
-    echo "Current git tag is not a release tag, exiting"
-    exit 1
+    GIT_TAG=${GIT_LATEST_TAG}
+else
+    GIT_TAG=${GIT_DESCRIBED_TAG}
 fi
 
-# Remove the "v" from the git tag, to match Rust convention
-GIT_TAG=${GIT_TAG:1}
+# Check the tag is not empty, else exit
+if [[ -z "${GIT_TAG}" ]];
+then
+    echo "Error: Could not get git tag"
+    exit 1
+else
+    echo "Release tag: ${GIT_TAG}"
+fi
+
+# Get the python package version from the setup.py file
+# This is the version number that will be used in the wheel filename
+# e.g. jcan-0.1.5-cp38-abi3-manylinux2014_aarch64.whl 
+PYTHON_PACKAGE_VERSION=$(grep -oP '(?<=version=")[^"]*' "${SCRIPT_DIR}/jcan-python/setup.py")
 
 # Copy the wheel(s) to the release directory
-cp "${SCRIPT_DIR}/out/wheels/jcan-${GIT_TAG}-"*.whl "${SCRIPT_DIR}/release/"
+cp "${SCRIPT_DIR}/out/wheels/jcan-${PYTHON_PACKAGE_VERSION}-"*.whl "${SCRIPT_DIR}/release/"
+
+# Rename the wheel(s) to use the GIT_TAG version
+for file in "${SCRIPT_DIR}/release/jcan-${PYTHON_PACKAGE_VERSION}-"*.whl;
+do
+    echo "Renaming ${file} to ${file/jcan-${PYTHON_PACKAGE_VERSION}-/jcan-${GIT_TAG}-}"
+    mv "${file}" "${file/jcan-${PYTHON_PACKAGE_VERSION}-/jcan-${GIT_TAG}-}"
+done
 
 # For each subdirectory of out/release/, representing a <target>/jcan combination
 # we will copy the jcan library to the release directory with the name jcan_<target>
