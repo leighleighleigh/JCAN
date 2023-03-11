8d7a2ed0e24ce22807b657dca913a7204247530c
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-03-11 13:03:53 +1100

    Demo of zero-length can frames

--- utils/spam-jcan.py
+++ utils/spam-jcan.py
@@ -10,4 +10,6 @@ bus.open(channel)
 while True:
     f = jcan.Frame(0x1A3, [0xC,0x0,0xF,0xF,0xE,time.time_ns() % 64,time.time_ns() % 255])
     bus.send(f)
+    f = jcan.Frame(0x1A5, [])
+    bus.send(f)
 
