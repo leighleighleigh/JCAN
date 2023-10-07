8b77c1821caf27f234ec8cdcd620de1cb0108a97
Leigh Oliver <leighcharlesoliver@gmail.com>
2023-10-07 19:09:25 +1100

    Added interactive commands to the JCAN rust binary, to demonstrate open/closing repeatedly

--- jcan/src/main.rs
+++ jcan/src/main.rs
@@ -29,8 +29,9 @@ fn main()
     // Open the bus in async mode
     bus.open("vcan0".to_string(),256,256).expect("Failed to open bus.");
 
+    let mut run = true;
 
-    loop {
+    while run {
         // Print and wait a bit
         println!("Spinning...");
         let frames = bus.receive_from_thread_buffer().expect("Failed to spin bus.");
@@ -38,17 +39,42 @@ fn main()
         // Print received frames
         for frame in frames {
             println!("Received frame: {}", frame);
+
+            // If the frame ID 0x123, with payload '42' is received, quit.
+            if frame.get_id() == 0x123 {
+              if frame.get_data().len() == 1 {
+                let b0 = frame.get_data()[0];
+                match b0 {
+                  0x42 => {
+                    println!("Received quit command!");
+                    run = false;     
+                  },
+                  0x11 => {
+                    println!("Received restart command!");
+                    println!("bus.close()...");
+                    let _ = bus.close();
+
+                    println!("sleep 3");
+                    std::thread::sleep(std::time::Duration::from_millis(3000));
+
+                    println!("bus.open()");
+                    bus.open("vcan0".to_string(),256,256).expect("Failed to open bus.");
+                  },
+                  _ => {}
+                }
+              }
+            }
         }
 
         // Build frame
         let frame = JFrame::build(0x123, [0x10,0x20].to_vec()).unwrap();
         println!("{}", frame);
+
         // Send frame
         bus.send(frame).expect("Failed to send frame.");
 
         std::thread::sleep(std::time::Duration::from_millis(1000));
-        // // receive frame (blocking)
-        // let frame = bus.receive().expect("Failed to receive frame.");
-        // println!("{}", frame);
     }
+
+    let _ = bus.close();
 }
